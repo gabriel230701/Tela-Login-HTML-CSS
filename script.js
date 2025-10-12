c97380b1bd4cba8066ef8b80e7eb5b35caf10fa7
gabriel230701 <biel.esteterr@hotmail.com>
2025-10-12 10:58:25 -0300

    Tela nova

---
+++ script.js
@@ -0,0 +1,19 @@
+// script.js
+
+document.querySelector("form").addEventListener("submit", function (event) {
+    event.preventDefault(); // impede o envio padrão do formulário
+
+    const usuario = document.querySelector('input[type="text"]').value;
+    const senha = document.querySelector('input[type="password"]').value;
+
+    // Aqui você define o login válido (exemplo)
+    if (usuario === "admin" && senha === "1234") {
+        // salva o usuário (opcional)
+        localStorage.setItem("usuarioLogado", usuario);
+
+        // redireciona pra segunda página
+        window.location.href = "home.html";
+    } else {
+        alert("Usuário ou senha incorretos!");
+    }
+});
